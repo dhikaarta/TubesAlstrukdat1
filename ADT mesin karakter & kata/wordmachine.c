062477ed004feaa7f79662c640e4b445a0340ade
dhikaarta <16520019@std.stei.itb.ac.id>
2021-10-27 20:30:56 +0700

    added adt mesin kata

---
+++ ADT mesin karakter & kata/wordmachine.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "wordmachine.h"
+
+
+boolean endWord;
+Word currentWord;
+
+void ignoreBlankWORD()
+{
+    while(currentChar == BLANK){
+        adv();
+    }
+}
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : currentChar sembarang 
+   F.S. : currentChar ≠ BLANK atau currentChar = MARK */
+
+void startWORD(){
+    start();
+    ignoreBlankWORD();
+    if (currentChar == MARK){
+        endWord = true;
+    } else{
+        endWord = false;
+        copyWORD();
+    }
+}
+/* I.S. : currentChar sembarang 
+   F.S. : endWord = true, dan currentChar = MARK; 
+          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
+          currentChar karakter pertama sesudah karakter terakhir kata */
+
+void advWORD(){
+    ignoreBlankWORD();
+    if (currentChar == MARK){
+        endWord  = true;
+    } else {
+        copyWORD();
+        ignoreBlankWORD();
+    }
+}
+/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
+   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
+          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika currentChar = MARK, endWord = true.		  
+   Proses : Akuisisi kata menggunakan procedure copyWORD */
+
+void copyWORD()
+{
+    int count = 0;
+    while((currentChar != MARK) && (currentChar != BLANK)){
+        currentWord.contents[count] = currentChar;
+        adv();
+        count += 1;
+    }
+    if (count > CAPACITYWORD){
+        currentWord.length = CAPACITYWORD;
+    } else{
+        currentWord.length = count;
+    }
+
+    
+
+}
+/* Mengakuisisi kata, menyimpan dalam currentWord
+   I.S. : currentChar adalah karakter pertama dari kata
+   F.S. : currentWord berisi kata yang sudah diakuisisi; 
+          currentChar = BLANK atau currentChar = MARK; 
+          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
---
+++ main/menu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../ADT mesin karakter & kata/wordmachine.h"
+
+int main()
+{
+    char[CAPACITYWORD];
+    startWord();
+
+}
