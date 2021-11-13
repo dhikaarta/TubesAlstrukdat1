bacb80eb6b4a9a9a95461cd6ac8597e3fe499e18
jask-19 <16520214@std.stei.itb.ac.id>
2021-11-13 17:42:28 +0700

    kondisi speed boost kalau ada heavy item lain

--- GAME/ability/ability.c
+++ GAME/ability/ability.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include "ability.h"
 
-void speedBoost(TIME *t, int *moveFreq) {
-  if (*moveFreq <= 10) {
-    (*t).incTime = 0.5;
-  } else {
-    (*t).incTime = 1;
-    *moveFreq = 0;
-    printf("Ability Speed Boost telah hilang\n");
+void speedBoost(TIME *t, int *moveFreq, Stack b) {
+  int i;
+  boolean found = false;
+  if (IDX_TOP_STACK(b) > 0) {
+    for (i = IDX_TOP_STACK(b) - 1; i >= 0; i--) {
+      if (b.buffer[i].itemTASK == 'H') {
+        found = true;
+      } 
+    }
+  }
+  if (found == false) {
+    if (*moveFreq <= 10) {
+      (*t).incTime = 0.5;
+    } else {
+      (*t).incTime = 1;
+      *moveFreq = 0;
+    }
   }
 }
 
